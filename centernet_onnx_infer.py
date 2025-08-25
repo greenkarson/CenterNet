2f351a49153e97a8e5ab72532e12bb065aa3502b
greenkarson <347474969@qq.com>
2025-08-25 22:13:15 +0800

    test onnx

---
+++ centernet_onnx_infer.py
@@ -0,0 +1,260 @@
+import onnxruntime
+import torch
+import cv2
+import numpy as np
+from torch import nn
+
+def _nms(heat, kernel=3):
+    pad = (kernel - 1) // 2
+
+    hmax = nn.functional.max_pool2d(
+        heat, (kernel, kernel), stride=1, padding=pad)
+    keep = (hmax == heat).float()
+    return heat * keep
+
+def _gather_feat(feat, ind, mask=None):
+    dim  = feat.size(2)
+    ind  = ind.unsqueeze(2).expand(ind.size(0), ind.size(1), dim)
+    feat = feat.gather(1, ind)
+    if mask is not None:
+        mask = mask.unsqueeze(2).expand_as(feat)
+        feat = feat[mask]
+        feat = feat.view(-1, dim)
+    return feat
+
+def _transpose_and_gather_feat(feat, ind):
+    feat = feat.permute(0, 2, 3, 1).contiguous()
+    feat = feat.view(feat.size(0), -1, feat.size(3))
+    feat = _gather_feat(feat, ind)
+    return feat
+
+def _topk(scores, K=40):
+    batch, cat, height, width = scores.size()
+    topk_scores, topk_inds = torch.topk(scores.view(batch, cat, -1), K)
+
+    topk_inds = topk_inds % (height * width)
+    topk_ys   = (topk_inds / width).int().float()
+    topk_xs   = (topk_inds % width).int().float()
+
+    topk_score, topk_ind = torch.topk(topk_scores.view(batch, -1), K)
+    topk_clses = (topk_ind / K).int()
+    topk_inds = _gather_feat(topk_inds.view(batch, -1, 1), topk_ind).view(batch, K)
+    topk_ys = _gather_feat(topk_ys.view(batch, -1, 1), topk_ind).view(batch, K)
+    topk_xs = _gather_feat(topk_xs.view(batch, -1, 1), topk_ind).view(batch, K)
+
+    return topk_score, topk_inds, topk_clses, topk_ys, topk_xs
+
+def ctdet_decode(heat, wh, reg=None, cat_spec_wh=False, K=100):
+    print(heat.shape)
+    batch, cat, height, width = heat.size()
+
+    # heat = torch.sigmoid(heat)
+    # perform nms on heatmaps
+    heat = _nms(heat)
+    print(heat.shape)
+    scores, inds, clses, ys, xs = _topk(heat, K=K)
+    if reg is not None:
+        reg = _transpose_and_gather_feat(reg, inds)
+        reg = reg.view(batch, K, 2)
+        xs = xs.view(batch, K, 1) + reg[:, :, 0:1]
+        ys = ys.view(batch, K, 1) + reg[:, :, 1:2]
+    else:
+        xs = xs.view(batch, K, 1) + 0.5
+        ys = ys.view(batch, K, 1) + 0.5
+    wh = _transpose_and_gather_feat(wh, inds)
+    if cat_spec_wh:
+        wh = wh.view(batch, K, cat, 2)
+        clses_ind = clses.view(batch, K, 1, 1).expand(batch, K, 1, 2).long()
+        wh = wh.gather(2, clses_ind).view(batch, K, 2)
+    else:
+        wh = wh.view(batch, K, 2)
+    clses  = clses.view(batch, K, 1).float()
+    scores = scores.view(batch, K, 1)
+    bboxes = torch.cat([xs - wh[..., 0:1] / 2, 
+                        ys - wh[..., 1:2] / 2,
+                        xs + wh[..., 0:1] / 2, 
+                        ys + wh[..., 1:2] / 2], dim=2)
+    detections = torch.cat([bboxes, scores, clses], dim=2)
+    return detections
+
+def get_dir(src_point, rot_rad):
+    sn, cs = np.sin(rot_rad), np.cos(rot_rad)
+
+    src_result = [0, 0]
+    src_result[0] = src_point[0] * cs - src_point[1] * sn
+    src_result[1] = src_point[0] * sn + src_point[1] * cs
+
+    return src_result
+
+def get_affine_transform(center,
+                         scale,
+                         rot,
+                         output_size,
+                         shift=np.array([0, 0], dtype=np.float32),
+                         inv=0):
+    if not isinstance(scale, np.ndarray) and not isinstance(scale, list):
+        scale = np.array([scale, scale], dtype=np.float32)
+
+    scale_tmp = scale
+    src_w = scale_tmp[0]
+    dst_w = output_size[0]
+    dst_h = output_size[1]
+
+    rot_rad = np.pi * rot / 180
+    src_dir = get_dir([0, src_w * -0.5], rot_rad)
+    dst_dir = np.array([0, dst_w * -0.5], np.float32)
+
+    src = np.zeros((3, 2), dtype=np.float32)
+    dst = np.zeros((3, 2), dtype=np.float32)
+    src[0, :] = center + scale_tmp * shift
+    src[1, :] = center + src_dir + scale_tmp * shift
+    dst[0, :] = [dst_w * 0.5, dst_h * 0.5]
+    dst[1, :] = np.array([dst_w * 0.5, dst_h * 0.5], np.float32) + dst_dir
+
+    src[2:, :] = get_3rd_point(src[0, :], src[1, :])
+    dst[2:, :] = get_3rd_point(dst[0, :], dst[1, :])
+
+    if inv:
+        trans = cv2.getAffineTransform(np.float32(dst), np.float32(src))
+    else:
+        trans = cv2.getAffineTransform(np.float32(src), np.float32(dst))
+
+    return trans
+
+def affine_transform(pt, t):
+    new_pt = np.array([pt[0], pt[1], 1.], dtype=np.float32).T
+    new_pt = np.dot(t, new_pt)
+    return new_pt[:2]
+
+def get_3rd_point(a, b):
+    direct = a - b
+    return b + np.array([-direct[1], direct[0]], dtype=np.float32)
+
+def transform_preds(coords, center, scale, output_size):
+    target_coords = np.zeros(coords.shape)
+    trans = get_affine_transform(center, scale, 0, output_size, inv=1)
+    for p in range(coords.shape[0]):
+        target_coords[p, 0:2] = affine_transform(coords[p, 0:2], trans)
+    return target_coords
+
+def ctdet_post_process(dets, c, s, h, w, num_classes):
+    # dets: batch x max_dets x dim
+    # return 1-based class det dict
+    ret = []
+    for i in range(dets.shape[0]):
+        top_preds = {}
+        dets[i, :, :2] = transform_preds(
+            dets[i, :, 0:2], c[i], s[i], (w, h))
+        dets[i, :, 2:4] = transform_preds(
+            dets[i, :, 2:4], c[i], s[i], (w, h))
+        classes = dets[i, :, -1]
+        for j in range(num_classes):
+            inds = (classes == j)
+            top_preds[j + 1] = np.concatenate([
+                dets[i, inds, :4].astype(np.float32),
+                dets[i, inds, 4:5].astype(np.float32)], axis=1).tolist()
+        ret.append(top_preds)
+    return ret
+
+def ctdet_pre_process(dets, c, s, h, w, num_classes):
+    # dets: batch x max_dets x dim
+    # return 1-based class det dict
+    ret = []
+    for i in range(dets.shape[0]):
+        top_preds = {}
+        dets[i, :, :2] = transform_preds(
+            dets[i, :, 0:2], c[i], s[i], (w, h))
+        dets[i, :, 2:4] = transform_preds(
+            dets[i, :, 2:4], c[i], s[i], (w, h))
+        classes = dets[i, :, -1]
+        for j in range(num_classes):
+            inds = (classes == j)
+            top_preds[j + 1] = np.concatenate([
+                dets[i, inds, :4].astype(np.float32),
+                dets[i, inds, 4:5].astype(np.float32)], axis=1).tolist()
+        ret.append(top_preds)
+    return ret
+
+def yolo_preprocess(image, infer_shape):
+    mean = np.array([0.485,0.456,0.406], dtype=np.float32)
+    mean = mean.reshape((3, 1, 1))
+    std = np.array([0.229,0.224,0.225], dtype=np.float32)
+    std = std.reshape((3, 1, 1))
+
+    height, width = image.shape[:2]
+    ratio_h, ratio_w = infer_shape[0] / height, infer_shape[1] / width
+    image = cv2.resize(
+        image, (0, 0),
+        fx=ratio_w,
+        fy=ratio_h,
+        interpolation=cv2.INTER_LINEAR)
+    image = np.ascontiguousarray(image.transpose(2, 0, 1))
+    image = image.astype(np.float32) / 255
+    image -= mean
+    image /= std
+    return image[np.newaxis], (ratio_w, ratio_h)
+
+def pre_process(image, scale, meta=None):
+    height, width = image.shape[0:2]
+    new_height = int(height * scale)
+    new_width  = int(width * scale)
+
+    mean = np.array([0.485,0.456,0.406], dtype=np.float32)
+    std = np.array([0.229,0.224,0.225], dtype=np.float32)
+
+    mean = np.array(mean, dtype=np.float32).reshape(1, 1, 3)
+    std = np.array(std, dtype=np.float32).reshape(1, 1, 3)
+    
+    down_ratio = 4
+
+    # if self.opt.fix_res:
+    #     inp_height, inp_width = self.opt.input_h, self.opt.input_w
+    #     c = np.array([new_width / 2., new_height / 2.], dtype=np.float32)
+    #     s = max(height, width) * 1.0
+    # else:
+    inp_height = (new_height | 31) + 1
+    inp_width = (new_width | 31) + 1
+    c = np.array([new_width // 2, new_height // 2], dtype=np.float32)
+    s = np.array([inp_width, inp_height], dtype=np.float32)
+
+    trans_input = get_affine_transform(c, s, 0, [inp_width, inp_height])
+    resized_image = cv2.resize(image, (new_width, new_height))
+    inp_image = cv2.warpAffine(resized_image, trans_input, (inp_width, inp_height), flags=cv2.INTER_LINEAR)
+    inp_image = ((inp_image / 255. - mean) / std).astype(np.float32)
+
+    images = inp_image.transpose(2, 0, 1).reshape(1, 3, inp_height, inp_width)
+    # if self.opt.flip_test:
+    #     images = np.concatenate((images, images[:, :, :, ::-1]), axis=0)
+    # images = torch.from_numpy(images)
+    meta = {'c': c, 's': s, 
+            'out_height': inp_height // down_ratio, 
+            'out_width': inp_width // down_ratio}
+    return images, meta
+
+if __name__ == "__main__":
+    img_path = '/mnt/quedoulin/github_code/yolov5/data/images/bus.jpg'
+    model_file = 'ctdet_helmet_opset9.onnx'
+
+    img = cv2.imread(img_path)
+    image_h, image_w = img.shape[:2]
+    shape = (512, 512)
+    num_classes = 2
+    scale = 1
+    meta = {}
+    _, meta = pre_process(img, scale, meta)
+    blob, (ratio_w, ratio_h) = yolo_preprocess(img, shape)
+    session = onnxruntime.InferenceSession(model_file, None)
+    output = session.run(None, {session.get_inputs()[0].name: blob})
+    print(len(output))
+    hm, wh, reg = output
+    print(output[0].shape,output[1].shape,output[2].shape)
+    dets = ctdet_decode(torch.from_numpy(hm), torch.from_numpy(wh), torch.from_numpy(reg), cat_spec_wh=False, K=100)
+    dets = dets.detach().cpu().numpy()
+    dets = dets.reshape(1, -1, dets.shape[2])
+    dets = ctdet_post_process(
+        dets.copy(), [meta['c']], [meta['s']],
+        meta['out_height'], meta['out_width'], num_classes)
+    for j in range(1, num_classes + 1):
+        dets[0][j] = np.array(dets[0][j], dtype=np.float32).reshape(-1, 5)
+        dets[0][j][:, :4] /= scale
+    print(dets)
